c7d1c37484ba54945ecfb88252f6a7038dcb2b30
linder3hs <linderhassinger00@gmail.com>
2021-03-18 11:32:35 -0500

    first commit

---
+++ semana2.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var str: String = "Hello, playground"
+
+let number: Int = 10
+
+let 😬 = 20
+
+let 🤥 = 40
+
+var sum = 😬 + 🤥
+
+typealias char = Character
+let letra: char = "G"
+
+
+
+var name = "Linder"
+var last_name = "Hassinger"
+
+let concatenacion = "Hola mi nombre es \(name) y mi apellido es \(last_name)"
+
+typealias entero = Int
+
+let numberAny: entero = 10
+
+print(name + last_name)
+
+print(name + String(number))
+
+var strName:String?
+
+strName = "Pepito"
+
+var numberOne: Int = 100
+
+var arrayInts: [Int] = [1, 2, 3, 4]
+
+var arrayNames: [String] = ["Pepito", "Juanita"]
+
+var diccionaryData: [String:String] = ["key" : "1", "key2" : "2"]
+
+diccionaryData["key2"]
+
+diccionaryData.count
